1b7e4f594c4fafec27ec42fb3360e222daf52108
Tobias Frauenfelder <tobias.frauenfelder@gmail.com>
2023-05-04 16:12:03 +0200

    Display real error message in frontend

--- lottery-frontend/src/App.js
+++ lottery-frontend/src/App.js
@@ -32,9 +32,9 @@ function App() {
     loadContract();
   }, []);
 
-  useEffect(()=>{
-    async function getVariables(){
-      if(!contract){
+  useEffect(() => {
+    async function getVariables() {
+      if (!contract) {
         return
       }
       const winner = await contract.methods.winner().call();
@@ -42,7 +42,6 @@ function App() {
       const maxPlayers = await contract.methods.ticketCount().call();
       const accounts = await web3.eth.getAccounts();
       const nextAddress = accounts[Math.floor(Math.random() * accounts.length)]
-      console.log(typeof nextAddress)
       setLastWinner(winner)
       setPlayers(players)
       setMaxPlayers(maxPlayers)
@@ -50,7 +49,7 @@ function App() {
     }
     getVariables()
 
-  },[contract, countBuy])
+  }, [contract, countBuy])
 
   useEffect(() => {
     const fetchParticipants = async () => {
@@ -69,7 +68,7 @@ function App() {
     fetchParticipants();
   }, [contract, countBuy]);
 
- 
+
 
   // Call a function on the smart contract when a button is clicked
   async function handleClick() {
@@ -77,46 +76,50 @@ function App() {
       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
       const userAddress = accounts[0];
       setAddress(userAddress);
-      try{
-    const result = await contract.methods.buyTicket().send({ 
-      from: userAddress,
-      value: web3.utils.toWei('1', 'ether'),
-      gas: 3000000
-     });
-    
-  }catch(e){
-    //console.log(e.receipt)
-    alert("You are not allowed to buy multiple Tickets with one Wallet.")
-  }
-    setCountBuy(countBuy+1)
-    }else{
+      try {
+        const result = await contract.methods.buyTicket().send({
+          from: userAddress,
+          value: web3.utils.toWei('1', 'ether'),
+          gas: 3000000
+        });
+
+      } catch (e) {
+        const tx = await web3.eth.getTransaction(e.receipt.transactionHash)
+        try {
+          var result = await web3.eth.call(tx, tx.blockNumber)
+        } catch (e) {
+          alert(e.message.substring(66))
+        }
+      }
+      setCountBuy(countBuy + 1)
+    } else {
       alert("You need to have a Wallet to buy a Ticket, for example Metamask")
     }
   }
-  
+
   return (
     <div className='flex flex-row justify-center pt-3 bg-gray-900 min-h-screen items-start text-white'>
-    <div className='lg:w-0.8 p-5 rounded-lg bg-gray-800 drop-shadow-lg'>
-      
-      <div className='flex flex-col space-y-3'>
-      <h1 className="font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Zeus Lottery</h1>
-        <Stat description={"Last Winner"} value={lastWinner}></Stat>
-      <div className='flex flex-row justify-between gap-4'>
-        <div className='flex-col flex justify-between gap-4'>
-      <Stat description={"Tickets in the pool"} value={players.length}></Stat>
-      <Stat description={participants.length === 0 ? "Your Chance of winning: 0%" : "Your Chance of winning: " + (100/participants.length * (participants.filter(participant => participant.toLowerCase() === address.toLowerCase())).length).toString()+ '%'}></Stat>
-      <Stat description={"Maximum Tickets"} value={maxPlayers}></Stat>
-      <Stat description={"Tickets left"} value={maxPlayers-participants.length}></Stat>
+      <div className='lg:w-0.8 p-5 rounded-lg bg-gray-800 drop-shadow-lg'>
+
+        <div className='flex flex-col space-y-3'>
+          <h1 className="font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Zeus Lottery</h1>
+          <Stat description={"Last Winner"} value={lastWinner}></Stat>
+          <div className='flex flex-row justify-between gap-4'>
+            <div className='flex-col flex justify-between gap-4'>
+              <Stat description={"Tickets in the pool"} value={players.length}></Stat>
+              <Stat description={participants.length === 0 ? "Your Chance of winning: 0%" : "Your Chance of winning: " + (100 / participants.length * (participants.filter(participant => participant.toLowerCase() === address.toLowerCase())).length).toString() + '%'}></Stat>
+              <Stat description={"Maximum Tickets"} value={maxPlayers}></Stat>
+              <Stat description={"Tickets left"} value={maxPlayers - participants.length}></Stat>
+            </div>
+            <PieChart description={"Participants"} list={participants}></PieChart>
+
+          </div>
+          {/* <TextField label="Address" type="text" onInputChange={handleInputChange} value={nextAddress} /> */}
+          <button className='rounded-lg p-3 bg-black text-white uppercase' onClick={handleClick}>Buy a ticket for the lottery</button>
+        </div>
+
+
       </div>
-      <PieChart description={"Participants"} list={participants}></PieChart>
-      
-      </div>
-     {/* <TextField label="Address" type="text" onInputChange={handleInputChange} value={nextAddress} /> */}
-      <button className='rounded-lg p-3 bg-black text-white uppercase' onClick={handleClick}>Buy a ticket for the lottery</button>
-      </div>
-      
-      
-    </div>
     </div>
   );
 }
